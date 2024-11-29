3d39929e587970178b0ac38ded9c3a413a29bfd5
Lilian BROUCA <lilianbrouca1302@gmail.com>
2024-11-29 10:20:24 +0100

    Config API request for the shop and send msg on discord

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 const { Events } = require('discord.js');
-const { client_token_helloasso, salon_id } = require('../config.json');
+const { client_secret_helloasso, client_id_helloasso, client_token_helloasso, salon_id } = require('../config.json');
 
 async function getInfoShop() {
     const url = 'https://api.helloasso.com/v5/organizations/atstri/forms/Shop/merch-et-goodies-2025/payments?pageIndex=1&pageSize=20';
@@ -19,25 +19,124 @@ async function getInfoShop() {
     return orders;
 }
 
+async function compareInfoShop(oldHistory, newHistory) {
+    if (JSON.stringify(oldHistory) !== JSON.stringify(newHistory)) {
+        const orders1 = new Set(oldHistory.data.map(order => order.order.id));
+        const newOrders = newHistory.data.filter(order => !orders1.has(order.order.id));
+
+        // Créer un dictionnaire pour associer les IDs des produits à leurs noms
+        const itemMap = {};
+        items.data.forEach(item => {
+            itemMap[item.id] = item.name;
+        });
+
+        // Calculer le total détaillé par produit
+        const productQuantities = {};
+
+        newOrders.forEach(order => {
+            order.items.forEach(item => {
+                const productName = itemMap[item.id] || `Produit ID ${item.id}`; // Nom du produit ou ID si non trouvé
+                const quantity = 1; // Chaque ligne représente 1 unité
+            
+                if (!productQuantities[productName]) {
+                    productQuantities[productName] = 0;
+                }
+                productQuantities[productName] += quantity;
+            });
+        });
+
+        const channel = client.channels.cache.get(salon_id);
+        channel.send('## Nouvelle commande !');
+        Object.entries(productQuantities).forEach(([productName, total]) => {
+            channel.send(`${productName} : ${total}`);
+        });
+        return true;
+    } else {
+        console.log('Pas de nouvelle commande');
+        return false;
+    }
+}
+
+async function getNewToken() {
+    const encodedParams = new URLSearchParams();
+    encodedParams.set('grant_type', 'client_credentials');
+    encodedParams.set('client_id', client_id_helloasso);
+    encodedParams.set('client_secret', client_secret_helloasso);
+
+    const url = 'https://api.helloasso.com/oauth2/token';
+    const options = {
+        method: 'POST',
+        headers: {
+            accept: 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: encodedParams
+    };
+
+    // reponse api dans une variable
+    let response = await fetch(url, options);
+    let jsonData = await response.json();
+    let token = jsonData.access_token;
+    let refreshToken = jsonData.refresh_token;
+    updateConfig(token, refreshToken);
+}
+
+async function refreshToken() {
+    const config = require('../config.json');
+    const encodedParams = new URLSearchParams();
+    encodedParams.set('grant_type', 'refresh_token');
+    encodedParams.set('client_id', client_id_helloasso);
+    encodedParams.set('refresh_token', config.refresh_token_helloasso);
+
+    const url = 'https://api.helloasso.com/oauth2/token';
+    const options = {
+        method: 'POST',
+        headers: {
+            accept: 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: encodedParams
+    };
+
+    // reponse api dans une variable
+    let response = await fetch(url, options);
+    let jsonData = await response.json();
+    let token = jsonData.access_token;
+    let refreshToken = jsonData.refresh_token;
+    console.log('Nouveau token');
+
+    updateConfig(token, refreshToken);
+}
+
+async function updateConfig(token, refresh_token) {
+    const fs = require('fs');
+    const config = require('../config.json');
+    config.client_token_helloasso = token;
+    config.refresh_token_helloasso = refresh_token;
+    fs.writeFile('./config.json', JSON.stringify(config, null, 2), (err) => {
+        if (err) {
+            console.log('Erreur lors de la mise à jour du fichier config.json');
+        } else {
+            console.log('Fichier config.json mis à jour');
+        }
+    });
+}
+
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+        getNewToken();
         // Récupérer les informations du shop
         let latestOrder = await getInfoShop();
         setInterval(async () => {
             let newOrder = await getInfoShop();
             //comparer les deux objets JSON
-            if (JSON.stringify(latestOrder) !== JSON.stringify(newOrder)) {
-                // si les objets sont différents, mettre à jour la dernière commande
+            if(compareInfoShop(latestOrder, newOrder)) {
                 latestOrder = newOrder;
-                // envoyer un message dans un salon
-                const channel = client.channels.cache.get(salon_id);
-                channel.send('Nouvelle commande !');
-            } else {
-                console.log('Pas de nouvelle commande');
             }
-        }, 5 * 60 * 1000); // 5 minutes en millisecondes
+            refreshToken();
+        }, 15 * 60 * 1000); // 15 minutes en millisecondes
 	},
 };
